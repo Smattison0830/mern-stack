ab76363a4c25c04239d573b7533ba9bf725617d5
Spencer Mattison <spenceramattison@gmail.com>
2022-05-17 17:24:52 +0900

    Create GoalList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import NewGoal from '.components/NewGoal'
+import GoalList from './components/GoalList'
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="course-goals">
+      <GoalList />
 
     </div>
   );
---
+++ src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './GoalList.css'
+
+
+export default function NewGoal() {
+    return (
+        <ul className="goal-list">
+            <li>Finish this course!</li>
+            <li>Learn how to create nice MERN websites</li>
+            <li>GET A JOB!</li>
+        </ul>
+    );
+};
