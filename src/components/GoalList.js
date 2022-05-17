b754d9a8e1d346063f46403a0fd60e49568be191
Spencer Mattison <spenceramattison@gmail.com>
2022-05-17 19:29:03 +0900

    Create NewGoal.js and NewGoal.css

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,13 +1,12 @@
 import React from "react";
-import './GoalList.css'
+import "./GoalList.css";
 
-export default function GoalList( props ) {
-
-    return (
-        <ul className="goal-list">
-            {props.goals.map(goal => {
-                return <li key={goal.id}>{goal.text}</li>
-            })}
-        </ul>
-    );
-};
+export default function GoalList(props) {
+  return (
+    <ul className='goal-list'>
+      {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>;
+      })}
+    </ul>
+  );
+}
---
+++ src/components/NewGoal.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./NewGoal.css";
+
+export default function NewGoal() {
+  return (
+    <form className='new-goal'>
+      <input type='text'></input>
+      <button type='submit'></button>
+    </form>
+  );
+}
